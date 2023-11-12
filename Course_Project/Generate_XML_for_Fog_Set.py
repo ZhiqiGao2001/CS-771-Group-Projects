801bc01c235a57a5383bf755e3d338dc9fcd6d99
ZhiqiGao-Leo <zgao93@wisc.edu>
2023-11-12 02:04:31 -0600

    缩减XML的脚本

--- Course_Project/Generate_XML_for_Fog_Set.py
+++ Course_Project/Generate_XML_for_Fog_Set.py
@@ -1,4 +1,32 @@
 import xml.etree.ElementTree as ET
+import os
+import re
+
+
+def get_folder_names(parent_folder):
+    # Get a list of all items in the parent folder
+    items = os.listdir(parent_folder)
+    # Filter out only folders
+    # folder_names = [parent_folder+'/'+item for item in items]
+    return items
+
+
+def extract_numbers_from_filenames(folder_path):
+    # Check if the folder path exists
+    if not os.path.exists(folder_path):
+        print(f"The specified folder path '{folder_path}' does not exist.")
+        return
+
+    # Get a list of all files in the folder
+    files = os.listdir(folder_path)
+
+    # Use regular expression to extract numbers from file names
+    pattern = re.compile(r'img(\d+).jpg', re.IGNORECASE)
+
+    # Extract numbers and remove leading zeros
+    numbers = [int(match.group(1)) for file in files if (match := pattern.match(file))]
+
+    return numbers
 
 
 def filter_frames(xml_path, selected_frame_numbers, output_path):
@@ -21,8 +49,38 @@ def filter_frames(xml_path, selected_frame_numbers, output_path):
     new_tree = ET.ElementTree(new_root)
     new_tree.write(output_path, encoding="utf-8", xml_declaration=True)
 
-# Example: Keep frames with numbers 1, 3, and 5
-selected_frames = [51, 101, 151, 201, 251]
-input_path = 'Dataset/DETRAC-Test-Annotations-XML/MVI_39031.xml'
-output_path = "Truncate_MVI_39031.xml"
-filter_frames(input_path, selected_frames, output_path)
+
+# selected_frames = [51, 101, 151, 201, 251]
+# input_path = 'Dataset/DETRAC-Test-Annotations-XML/MVI_39031.xml'
+# output_path = "Truncate_MVI_39031.xml"
+# filter_frames(input_path, selected_frames, output_path)
+
+train_folder_path = "Dataset/UA-DETRAC/gt/train"
+test_folder_path = "Dataset/UA-DETRAC/gt/test"
+XML_train_folder_path = 'Dataset/DETRAC-Train-Annotations-XML'
+XML_test_folder_path = 'Dataset/DETRAC-Test-Annotations-XML'
+output_train_folder_path = 'Dataset/UA-DETRAC-Annotations/train'
+output_test_folder_path = 'Dataset/UA-DETRAC-Annotations/test'
+
+# Get the list of folder names
+train_folder_list = get_folder_names(train_folder_path)
+test_folder_list = get_folder_names(test_folder_path)
+
+for train_folder in train_folder_list:
+    # Get the list of frame numbers
+    train_frame_numbers = extract_numbers_from_filenames(train_folder_path + '/' + train_folder)
+    # Get the corresponding XML file
+    xml_file = XML_train_folder_path + '/' + train_folder + '.xml'
+    output_xml_file = output_train_folder_path + '/' + train_folder + '.xml'
+    # Filter the XML files
+    filter_frames(xml_file, train_frame_numbers, output_xml_file)
+
+
+for test_folder in test_folder_list:
+    # Get the list of frame numbers
+    test_frame_numbers = extract_numbers_from_filenames(test_folder_path + '/' + test_folder)
+    # Get the corresponding XML file
+    xml_file = XML_test_folder_path + '/' + test_folder + '.xml'
+    output_xml_file = output_test_folder_path + '/' + test_folder + '.xml'
+    # Filter the XML files
+    filter_frames(xml_file, test_frame_numbers, output_xml_file)
