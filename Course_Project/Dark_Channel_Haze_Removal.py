0159f06ca8656d7f717e20eecb9587010bf166f0
ZhiqiGao-Leo <zgao93@wisc.edu>
2023-11-13 13:51:46 -0600

    dark channel code

--- Course_Project/Dark_Channel_Haze_Removal.py
+++ Course_Project/Dark_Channel_Haze_Removal.py
@@ -1,6 +1,7 @@
 #  Reference: https://blog.csdn.net/wsp_1138886114/article/details/95012769
 import cv2
 import numpy as np
+import os
 
 
 def zmMinFilterGray(src, r=7):
@@ -52,7 +53,52 @@ def deHaze(m, r=81, eps=0.001, w=0.95, maxV1=0.80, bGamma=False):
     return Y
 
 
+def dehaze_folder(input_folder, output_folder):
+    # Create output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Iterate through all JPG files in the input folder
+    for file_name in os.listdir(input_folder):
+        if file_name.endswith(".jpg"):
+            input_path = input_folder + '/' + file_name
+            output_path = output_folder + '/' + file_name
+
+            # Read the image, apply deHaze, and save the result
+            dehazed_img = deHaze(cv2.imread(input_path) / 255.0) * 255
+            cv2.imwrite(output_path, dehazed_img)
+
+
+def get_folder_names(parent_folder):
+    # Get a list of all items in the parent folder
+    items = os.listdir(parent_folder)
+    # Filter out only folders
+    # folder_names = [parent_folder+'/'+item for item in items]
+    return items
+
+
 if __name__ == '__main__':
-    img_path = "Dataset/UA-DETRAC/hazy/test/MVI_39031_241_0.02/img00301.jpg"
-    m = deHaze(cv2.imread(img_path) / 255.0) * 255
-    cv2.imwrite('result.png', m)
+    # img_path = "Dataset/UA-DETRAC/hazy/train/MVI_20011_229_0.03/img00386.jpg"
+    # m = deHaze(cv2.imread(img_path) / 255.0) * 255
+    # cv2.imwrite('result.png', m)
+
+    train_folder_path = "Dataset/UA-DETRAC/hazy/train"
+    test_folder_path = "Dataset/UA-DETRAC/hazy/test"
+    train_output_folder_path = "Dataset/UA-DETRAC/dehaze_DarkChannel/train"
+    test_output_folder_path = "Dataset/UA-DETRAC/dehaze_DarkChannel/test"
+
+    train_folder_list = get_folder_names(train_folder_path)
+    test_folder_list = get_folder_names(test_folder_path)
+    print(len(train_folder_list), len(test_folder_list))
+    for folder_name in train_folder_list:
+        input_folder = train_folder_path + '/' + folder_name
+        output_folder = train_output_folder_path + '/' + folder_name
+        print(input_folder, output_folder)
+        dehaze_folder(input_folder, output_folder)
+
+    for folder_name in test_folder_list:
+        input_folder = test_folder_path + '/' + folder_name
+        output_folder = test_output_folder_path + '/' + folder_name
+        print(input_folder, output_folder)
+        dehaze_folder(input_folder, output_folder)
+
